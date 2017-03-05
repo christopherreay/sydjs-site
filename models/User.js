45238e485fdc64740b65b79be1b6a0d67432947f
Jay Dazy <christopherreay@gmail.com>
2017-03-05 15:30:32 +0000

    github auth working, a logo

--- models/User.js
+++ models/User.js
@@ -68,39 +68,7 @@ User.add({
 			accessToken: { type: String, label: 'Access Token', dependsOn: deps.github },
 			refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.github }
 		},
-		facebook: {
-			isConfigured: { type: Boolean, label: 'Facebook has been authenticated' },
-
-			profileId: { type: String, label: 'Profile ID', dependsOn: deps.facebook },
-
-			username: { type: String, label: 'Username', dependsOn: deps.facebook },
-			avatar: { type: String, label: 'Image', dependsOn: deps.facebook },
-
-			accessToken: { type: String, label: 'Access Token', dependsOn: deps.facebook },
-			refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.facebook }
-		},
-		google: {
-			isConfigured: { type: Boolean, label: 'Google has been authenticated' },
-
-			profileId: { type: String, label: 'Profile ID', dependsOn: deps.google },
-
-			username: { type: String, label: 'Username', dependsOn: deps.google },
-			avatar: { type: String, label: 'Image', dependsOn: deps.google },
-
-			accessToken: { type: String, label: 'Access Token', dependsOn: deps.google },
-			refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.google }
-		},
-		twitter: {
-			isConfigured: { type: Boolean, label: 'Twitter has been authenticated' },
-
-			profileId: { type: String, label: 'Profile ID', dependsOn: deps.twitter },
-
-			username: { type: String, label: 'Username', dependsOn: deps.twitter },
-			avatar: { type: String, label: 'Image', dependsOn: deps.twitter },
-
-			accessToken: { type: String, label: 'Access Token', dependsOn: deps.twitter },
-			refreshToken: { type: String, label: 'Refresh Token', dependsOn: deps.twitter }
-		}
+		
 	}
 }, 'Meta', {
 	talkCount: { type: Number, default: 0, noedit: true },
