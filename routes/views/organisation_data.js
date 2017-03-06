6747b9a20ab4f1d436e3af02713f5cb58b21c1f6
Jay Dazy <christopherreay@gmail.com>
2017-03-06 13:47:15 +0000

    took  me that time to pass a variable through Jade.

---
+++ routes/views/organisation_data.js
@@ -0,0 +1,23 @@
+var keystone = require('keystone');
+
+var Organisation = keystone.list('Organisation');
+
+exports = module.exports = function(req, res) {
+  
+  var view = new keystone.View(req, res),
+    locals = res.locals;
+
+  console.log(req.query.id);
+  locals.orgID = req.query.orgID
+  
+  // locals.section = 'members';
+  
+  //view.query('organisations', Organisation.model.find({"_id": req.query.orgID}).sort('name'), 'members');
+
+  Organisation.model.findOne({"_id": organisationID})
+    .exec
+    ( function(err, organisation)
+      { res.json({"organisation": organisation});
+      }
+    );  
+}
